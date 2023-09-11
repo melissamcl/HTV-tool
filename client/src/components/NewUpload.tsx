2fd374fd412ea8908a2a3bfd92a967ce723b6598
melissamcl <mcl.melissa.m@gmail.com>
2023-09-11 14:09:32 -0400

    image upload basics, not functional

--- client/src/components/NewUpload.tsx
+++ client/src/components/NewUpload.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NewUpload() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleFileUpload = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log('uploaded');
+  };
+
   return (
     <div>
       <h1>New Upload</h1>
-      <button>Submit</button>
+      <input
+        type="file"
+        accept=".png, .jpg, .jpeg, .gif, .svg, .bmp, .tiff"
+        onChange={handleFileChange}
+      ></input>
+      <button onClick={handleFileUpload}>Upload</button>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -35,12 +35,16 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(js|ts|tsx)$/,
+        test: /\.(js|ts)x$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
           },
         },
       },

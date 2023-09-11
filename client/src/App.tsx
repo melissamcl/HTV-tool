b5fb87ca7da0447e00343061cdd31f2c1408ddc1
melissamcl <mcl.melissa.m@gmail.com>
2023-09-11 14:42:26 -0400

    react router added, basic setup for inventory page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
 import './styles/main.scss';
 import NewUpload from './components/NewUpload';
+import Inventory from './components/Inventory';
 
 function App() {
-  return <NewUpload />;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          {/* <Route path="/" element={Home} /> */}
+          <Route path="/new-upload" element={<NewUpload />} />
+          <Route path="/inventory" element={<Inventory />} />
+          {/* <Route path="*" element={<NotFound />} /> 404 Not Found */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/Inventory.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function Inventory() {
+  // iterate over all colors in db
+  // for each color, create circle with bg color set to hex
+  // on hover, display properties (brand + style, color name)
+
+  // options at top of page to filter by brand/style
+  return (
+    <div>
+      <h1>Inventory</h1>
+    </div>
+  );
+}
+
+export default Inventory;
